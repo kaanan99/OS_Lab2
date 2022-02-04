b588e1982e6b2397229bc292b63e966ce788592c
Kaanan Kharwa <kaanan99@gmail.com>
2022-02-04 11:28:05 -0800

    Ran test cases and it SEEMS correct

--- schedSim.py
+++ schedSim.py
@@ -17,6 +17,10 @@ class Job:
     def __lt__(self, other):
         return self.run_time < other.run_time
 
+def print_debug(jobs):
+    for x in jobs:
+        print(x)
+
 def sort_arrival(job):
     return job.arrival
 
@@ -41,22 +45,26 @@ def round_robin(jobs, q):
     current_job = 0
     next_job = 1
     job_runtime = 0
-    jobs[0].start = time
     while len(running_jobs) > 0:
-        # Next Job
-        if job_runtime == q:
-            current_job += 1
-            if current_job == len(running_jobs):
-                current_job = 0
-            job_runtime = 0
-            if running_jobs[current_job].start == -1:
-                running_jobs[current_job].start = time
+        # Update start time
+        if running_jobs[current_job].start == -1:
+            running_jobs[current_job].start = time
         # Adding New Jobs
         while next_job < len(jobs) and jobs[next_job].arrival == time:
             running_jobs.append(jobs[next_job])
             next_job += 1
+        # Updating runtime, q, and time
         running_jobs[current_job].run_time -= 1
         job_runtime += 1
+        time += 1
+        # Next Job
+        if job_runtime == q:
+            # Go to next job
+            current_job += 1
+            job_runtime = 0
+            # Check if at end of list
+            if current_job == len(running_jobs):
+                current_job = 0
         # Removing Job
         if running_jobs[current_job].run_time == 0:
             running_jobs[current_job].completed = time
@@ -64,7 +72,8 @@ def round_robin(jobs, q):
             if current_job >= len(running_jobs):
                 current_job = 0
             job_runtime = 0
-        time += 1
+
+
 
 
 def srtn(jobs):
@@ -77,7 +86,7 @@ def srtn(jobs):
         while next_job < len(jobs) and jobs[next_job].arrival == time:
             q.put(jobs[next_job])
             next_job += 1
-        # Increment time
+        # Get job with shortest time remaining
         current = q.get()
         # Update the start value
         if current.start == -1:
@@ -89,7 +98,8 @@ def srtn(jobs):
             q.put(current)
         # If completed mark down the time
         else:
-            current.completed = time
+            current.completed = time + 1
+        # Increment time
         time += 1
 
 def fifo(jobs):
@@ -103,7 +113,7 @@ def print_jobs(jobs):
     avg_turn_around = 0
     avg_wait = 0
     for job in jobs:
-        turn_around = job.completed - job.start
+        turn_around = job.completed - job.arrival
         wait = job.start - job.arrival
         print("Job {0} -- Turnaround {1:3.2f} Wait {2:3.2f}".format(job.name, turn_around, wait))
         avg_turn_around += turn_around
@@ -113,7 +123,7 @@ def print_jobs(jobs):
 def main():
     parser = argparse.ArgumentParser(description="Simulation of a scheduler")
     parser.add_argument("job_file", help= "Path to the file with the jobs")
-    parser.add_argument("-p", required=True, help= "Type of scheduler (SRTN, FIFO, or RR")
+    parser.add_argument("-p", required=False, help= "Type of scheduler (SRTN, FIFO, or RR")
     parser.add_argument("-q", required=False, help= "Time quantum value, defaults to 1")
     args = parser.parse_args()
     jobs = parse_file(args.job_file)
@@ -124,11 +134,12 @@ def main():
         if args.q == None:
             round_robin(jobs, 1)
         else:
-            round_robin(jobs, args.q)
-    elif alg == "FIFO":
+            round_robin(jobs, int(args.q))
+    elif alg == "FIFO" or  alg == None:
         fifo(jobs)
     else:
         raise Exception("Invalid algorithm")
+    #print_debug(jobs)
     print_jobs(sorted(jobs, key=sort_name))
 
 
